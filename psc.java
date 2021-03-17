b12af4fbbd8dcdc788a2b8383245797742b7a7b4
unknown <25ankitkumar99@gmail.com>
2021-03-17 17:05:56 +0530

    first commit

---
+++ psc.java
@@ -0,0 +1,21 @@
+package pascaltri;
+import java.util.Scanner;
+public class psc {
+	public static void main(String args[]) {
+	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a=sc.nextInt();
+		for(int i=1;i<=a;i++) {
+			for(int j=1;j<=i;j++) {
+			System.out.print(j);
+			System.out.print("\t");
+			
+		}
+			System.out.print("\n");
+		
+
+	}
+	}
+
+}
